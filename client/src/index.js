87077116db1cbad036b9a3dd1cd67abafcf72a01
lifrank1 <frank.li.865985@gmail.com>
2023-11-21 23:23:19 -0500

    connecting back/front end

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+//https://blog.nextideatech.com/how-to-get-started-with-the-mern-stack-a-comprehensive-guide/
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
