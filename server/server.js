b8c4520ee0aa1e8f35368386d7e0297013cc46a5
lifrank1 <frank.li.865985@gmail.com>
2023-12-31 16:26:57 -0500

    CORS debug

--- server/server.js
+++ server/server.js
@@ -5,20 +5,19 @@ const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 
- //node server.js client in server directory
- //lsof -i -P -n | grep LISTEN to find a new port
+//node server.js client in server directory
+//lsof -i -P -n | grep LISTEN to find a new port
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 
 // Get MongoDB driver connection
 const dbo = require("./db/conn");
- 
+
 app.listen(port, () => {
   // Perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
   });
   console.log(`Server is running on port: ${port}`);
 });
