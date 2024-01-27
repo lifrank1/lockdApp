f57fc9780cb47896d98b277090b641460616fb20
lifrank1 <frank.li.865985@gmail.com>
2024-01-26 23:30:45 -0500

    fixing connection

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,23 +1,32 @@
-const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db);
- 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 var _db;
- 
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(Db, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 //node server.js client in server directory
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
       // verify db object
-      if (db)
-      {
+      console.log("TESTING123.");
+      if (db) {
         _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
+        console.log("Successfully connected to MongoDB.");
       }
       return callback(err);
-         });
+    });
   },
- 
+
   getDb: function () {
     return _db;
   },
