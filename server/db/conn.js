4da5429d09fe97d7d48d82e49514af0c61d9bdf2
lifrank1 <frank.li.865985@gmail.com>
2024-01-28 23:37:14 -0500

    fixed connection to mongodb

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,33 +1,27 @@
-const Db = process.env.ATLAS_URI;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://frankli1:1DsbbqNkef4j3RXg@quickstickycluster0.5e52as4.mongodb.net/?retryWrites=true&w=majority";
 
-
-var _db;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(Db, {
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-//node server.js client in server directory
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // verify db object
-      console.log("TESTING123.");
-      if (db) {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB.");
-      }
-      return callback(err);
-    });
-  },
-
-  getDb: function () {
-    return _db;
+  connectToServer: async function (callback) {
+    try {
+      await client.connect();
+      await client.db("admin").command({ ping: 1 });
+      console.log("Successfully connected to MongoDB!");
+      return client.db("employees");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      return callback(error);
+    } finally {
+      await client.close();
+    }
   },
 };
